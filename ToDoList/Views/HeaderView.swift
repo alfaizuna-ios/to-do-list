625be88393e03a2363ff18aa7d19a9b2ad7cc531
alfaizuna-ios <alfaizuna.ios@gmail.com>
2024-10-08 22:31:19 +0700

    change LoginView

--- ToDoList/Views/HeaderView.swift
+++ ToDoList/Views/HeaderView.swift
@@ -9,7 +9,25 @@ import SwiftUI
 
 struct HeaderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            RoundedRectangle(cornerRadius: 0)
+                .foregroundStyle(Color.pink)
+                .rotationEffect(Angle(degrees: 15))
+            
+            VStack {
+                Text("To Do List")
+                    .font(.system(size: 50))
+                    .foregroundStyle(Color.white)
+                    .bold()
+                Text("get things done")
+                    .font(.system(size: 30))
+                    .foregroundStyle(Color.white)
+            }
+            .padding(.top, 30)
+        }
+        .frame(width: UIScreen.main.bounds.width * 3,
+               height: 300)
+        .offset(y: -100)
     }
 }
 
